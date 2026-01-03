810bb43a2004cc03ac33df10613703a89e3b7668
SuanYu2000 <115220795+SuanYu2000@users.noreply.github.com>
2026-01-03 18:32:48 +0800

    Update MARLoss_match.py

--- loss/MARLoss_match.py
+++ loss/MARLoss_match.py
@@ -8,7 +8,7 @@ from loss.Dist_mar_match import Dist_mar_match
 class MARLoss_match(nn.CrossEntropyLoss):
     def __init__(self, **options):
         super(MARLoss_match, self).__init__()
-        self.use_gpu = options['use_gpu'] # 没啥用，后期删
+        self.use_gpu = options['use_gpu'] 
         self.weight_pl = float(options['weight_pl'])
         self.temp = options['temp']
         self.Dist = Dist_mar_match(options['centroid_path'], gpu=0)
@@ -20,14 +20,14 @@ class MARLoss_match(nn.CrossEntropyLoss):
 
     def forward(self, x, y, labels=None):
 
-        # 动态获取最新中心（方向 × 模长）
+        
         centers = self.Dist.get_scaled_centers()
 
-        # 计算两个距离分支
+  
         dist_dot_p = self.Dist(x, center=centers, metric='dot')
         dist_l2_p = self.Dist(x, center=centers, metric='l2')
 
-        # 差值作为 logits
+    
         logits = dist_l2_p - dist_dot_p
 
         if labels is None:
@@ -35,10 +35,10 @@ class MARLoss_match(nn.CrossEntropyLoss):
 
         self.Dist.update_queue(x, labels)
 
-        # 交叉熵 loss
+   
         loss = F.cross_entropy(logits / self.temp, labels)
 
-        # 正则项：距离当前样本到其中心的欧式距离
+
         center_batch = centers[labels, :]
         _dis_known = (x - center_batch).pow(2).mean(1)
         target = torch.ones_like(_dis_known).to(x.device)
