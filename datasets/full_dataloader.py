d532293bbfa97cd2a3b19e64c4fb5e4631f0bd02
SuanYu2000 <115220795+SuanYu2000@users.noreply.github.com>
2026-01-03 18:28:55 +0800

    Update full_dataloader.py

--- datasets/full_dataloader.py
+++ datasets/full_dataloader.py
@@ -23,14 +23,14 @@ class CIFAR100_Cls(object):
 
         pin_memory = True if use_gpu else False
 
-        # 训练集
+        
         trainset = CIFAR100(root=dataroot, train=True, download=True, transform=train_transform)
         self.train_loader = torch.utils.data.DataLoader(
             trainset, batch_size=batch_size, shuffle=True,
             num_workers=num_workers, pin_memory=pin_memory,
         )
 
-        # 测试集
+        
         testset = CIFAR100(root=dataroot, train=False, download=True, transform=test_transform)
         self.test_loader = torch.utils.data.DataLoader(
             testset, batch_size=batch_size, shuffle=False,
@@ -44,7 +44,7 @@ class CIFAR100_Cls(object):
 
 class TinyImageNet_Cls(object):
     def __init__(self, dataroot='./data/tiny_imagenet/tiny-imagenet-200', use_gpu=True, num_workers=8, batch_size=128, img_size=64):
-        self.num_classes = 200  # Tiny-ImageNet 一共 200 类
+        self.num_classes = 200  
 
         train_transform = transforms.Compose([
             transforms.Resize((img_size, img_size)),
@@ -60,7 +60,7 @@ class TinyImageNet_Cls(object):
 
         pin_memory = True if use_gpu else False
 
-        # 训练集
+        
         train_dir = os.path.join(dataroot, 'train')
         trainset = ImageFolder(train_dir, transform=train_transform)
         self.train_loader = torch.utils.data.DataLoader(
@@ -68,7 +68,7 @@ class TinyImageNet_Cls(object):
             num_workers=num_workers, pin_memory=pin_memory,
         )
 
-        # 验证集（注意 Tiny-ImageNet 的 val 原始文件需要先通过官方脚本 organize_val.py 整理过）
+        
         val_dir = os.path.join(dataroot, 'val')
         testset = ImageFolder(val_dir, transform=test_transform)
         self.test_loader = torch.utils.data.DataLoader(
